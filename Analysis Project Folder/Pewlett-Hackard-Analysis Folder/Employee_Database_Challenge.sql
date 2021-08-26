2b1d3d6acb3f1a1e7bbad77ac13eae54ab0a9827
Venkateshbhakthavat <86161480+Venkateshbhakthavat@users.noreply.github.com>
2021-08-25 20:41:20 -0400

    Add files via upload

---
+++ Analysis Project Folder/Pewlett-Hackard-Analysis Folder/Employee_Database_Challenge.sql
@@ -0,0 +1,55 @@
+SELECT e.emp_no,
+   e.first_name,
+   e.last_name,
+   i.title,
+   i.from_date,
+   i.to_date
+INTO retirement_titles
+FROM employees AS e
+  INNER JOIN titles AS i
+    ON (e.emp_no = i.emp_no)
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY e.emp_no;
+
+SELECT DISTINCT ON (emp_no) emp_no, first_name, last_name, title
+INTO unique_titles
+FROM (SELECT emp_no,
+  first_name,
+  last_name,
+  to_date,
+  title
+  FROM
+  retirement_titles
+  ORDER BY emp_no, to_date DESC) ut
+  
+  select * From unique_titles;
+
+  SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+Select * From retiring_titles;
+
+
+
+Deliverables 2
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+  e.first_name,
+  e.last_name,
+  e.birth_date,
+  de.from_date,
+  de.to_date,
+  ti.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+  ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+  ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+select * From mentorship_eligibilty;
